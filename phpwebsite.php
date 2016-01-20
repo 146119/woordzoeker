463bca2d7f751635e7bed6b2b07b28744f11c17f
Erkan Yilmaz <146119@hpleerling.nl>
2016-01-20 13:21:14 +0100

    begin van de site

--- phpwebsite.php
+++ phpwebsite.php
@@ -6,6 +6,7 @@ and open the template in the editor.
 -->
 <html>
     <head>
+        <link rel="stylesheet" type="text/css" href="phpwebsite.css">
         <meta charset="UTF-8">
         <title></title>
     </head>
@@ -14,5 +15,27 @@ and open the template in the editor.
             include "bestand.php";
         // put your code here
         ?>
+        
+        <div class="a">
+<table class="b" border="1" style="width:10%">
+  <tr>
+    <td><form action="upload.php" method="post" enctype="multipart/form-data">
+    Select file to upload:
+    <input type="file" name="fileToUpload" id="fileToUpload">
+</form>
+
+    </td>
+  </tr>
+</table> 
+        
+<table class="c" border="1" style="width:10%">
+    <tr>
+        <td>
+            Dit wordt de woordzoeker
+        </td>
+    </tr>
+</table>
+            
+            </div>        
     </body>
 </html>
