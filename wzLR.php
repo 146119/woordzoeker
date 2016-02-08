14f0741d9268bab0a9650b7cf7250f4808beec88
Sam Groot <147179@hpleerling.nl>
2016-02-08 15:55:03 +0100

    wzLR verder

--- wzLR.php
+++ wzLR.php
@@ -2,9 +2,20 @@
 foreach ($wz as $regel) {
 // ---erkan----, ----sam-----, --yoni------, -boom-------, -----appel--, ---school---
 foreach($regel as $letter) {
-  if ()  
+  
 }
     
 }
 
+function zoekLR($wz,$w) {
+    zoekLR($wz, $w [0]); 
+    
+    $wz[0]= str_split('---erkan----');
+    $wz [1]= str_split('----sam-----');  
+    $wz [2]= str_split('--yoni------');
+    $wz [3]=str_split('-boom-------');
+    $wz [4]=str_split('-----appel--');
+    $wz [5]=str_split('---school---');
+    
+}
 ?>
