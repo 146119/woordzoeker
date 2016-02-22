c5a0a13a8c7b1c9f646a68069c764ca3fae17111
unknown <146119@WKS-HP-1747.ASG.LOCAL>
2016-02-22 16:01:07 +0100

    van - naar letters en alfabetr G UNIT

--- tabel.php
+++ tabel.php
@@ -20,8 +20,17 @@ function build_table($array) {
     return $html;
 }
   
+//minnetjes veranderen in letters
+function minnetjesNaarLetters($woordenzoeker) {
+    global $woordenzoeker;
+    $alfabet = array("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
+    foreach ($woordenzoeker as &$value) {
+        foreach ($value as &$value2) {
+            if ($value2 == "-") {
+                $value2 = $alfabet[rand(0, 25)];
+            }
+        }
+    }
+}
 
-$letters = array("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
-            );  
-         
 ?>
