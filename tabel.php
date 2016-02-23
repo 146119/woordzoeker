06a435762b747c640858348eb9b05e845d6bbbc6
Erkan Yilmaz <146119@hpleerling.nl>
2016-02-23 11:23:03 +0100

    splitsen van de woorden en het maken van de tabel

--- tabel.php
+++ tabel.php
@@ -1,36 +1,22 @@
 <?php
-//$wz = array($bestand);   //array van het bestand
-
+//functie om een tabel te kunnen maken van een array
 function build_table($array) {
     $html = '<table>';
-    $xcoördinaat = "0";
-    $ycoördinaat = "0";
-    foreach ($array as $key => $value)  {
+    $xcordinaat = "0";
+    $ycordinaat = "0";
+    foreach ($array as $key => $value) {
         $html .= '<tr>';
-        foreach ($value as $key2 => $value2)    {
-            $coördinaat = 'x' . $xcoördinaat . "y" . $ycoördinaat;
-            $html .= "<td class='$coördinaat'>" . $value2 . '</td>';
-            $xcoördinaat = $xcoördinaat + 1;
+        foreach ($value as $key2 => $value2) {
+            $cordinaat = "x" . $xcordinaat . "y" . $ycordinaat;
+            $html .= "<td class='$cordinaat'>" . $value2 . '</td>';
+            $xcordinaat = $xcordinaat + 1;
         }
-        $html .='<?tr>';
-        $ycoördinaat = $ycoördinaat + 1;
-        $xcoördinaat = "0";
+        $html .= '</tr>';
+        $ycordinaat = $ycordinaat + 1;
+        $xcordinaat = "0";
     }
     $html .= '</table>';
     return $html;
-}
-  
-//minnetjes veranderen in letters
-function minnetjesNaarLetters($woordenzoeker) {
-    global $woordenzoeker;
-    $alfabet = array("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
-    foreach ($woordenzoeker as &$value) {
-        foreach ($value as &$value2) {
-            if ($value2 == "-") {
-                $value2 = $alfabet[rand(0, 25)];
-            }
-        }
-    }
 }
 
 ?>
