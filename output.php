e898ef288e23f818364889fc021eabb42bc37920
thabomb31 <erkan_yilmaz-@hotmail.com>
2016-02-23 21:06:11 +0100

    output aangepast voor div

--- output.php
+++ output.php
@@ -1,4 +1,8 @@
-<?php
+
+
+<html>
+    <head>
+        <?php
 include 'Splitsen.php';
 include 'BestandToevoegen.php';
 include 'MinnetjesNaarLetters.php';
@@ -9,6 +13,7 @@ include 'PrintZoekwoorden.php';
 voegBestandToe();
 splitsen($woordenzoeker);
 minnetjesNaarLetters($woordenzoeker);
+
 $niveau = 2;
 if (isset($_POST["Niveau1"])) {
     $niveau = 1;
@@ -21,11 +26,7 @@ if (isset($_POST["Niveau2"])) {
 if (isset($niveau)) {
     horizontaalZoeken($woordenzoeker, $gesplitst, $niveau);
 }
-?>
-
-<html>
-    <head>
-     <?php jQueryEnPhpOpmaak($gevondenWoordenCoordinaten, $zoekwoorden); ?>
+jQueryEnPhpOpmaak($gevondenWoordenCoordinaten, $zoekwoorden); ?>
         <link rel="stylesheet" type="text/css" href="output.css">
         <meta charset="UTF-8">
         <title></title>
@@ -34,25 +35,20 @@ if (isset($niveau)) {
  </div>
     </head>
     <body>
- 
-            
-  
+        
         <?php
         bestandtoevoegenaanenuit();
         ?>
-        <div>
             <form action="Output.php" method="post">
                 <input id="knoppen" type="submit" name="Niveau1" value="links naar rechts"></input>
                 <input id="knoppen" type="submit" name="Niveau2" value="horizontaal"></input>
 
             </form>
-        </div>
-
-        <div id="woordzoekertabel">
-        <?php echo build_table($woordenzoeker); ?>   
-            <div id="woordenlijst">
+  
+        
+        <?php echo build_table($woordenzoeker); ?> 
             <?php printZoekwoorden($zoekwoorden); ?>
-            </div>
+          
        
     </body>
 </html>
